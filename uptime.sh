97752b1a43ac215dec138136f4b12e82d1aac5bc
faustouribe <faustouribe@gmail.com>
2026-01-16 18:00:28 -0600

    removed -v /app

--- uptime.sh
+++ uptime.sh
@@ -1,7 +1,6 @@
 docker run -d \
 -p 3001:3001 \
 -v /home/<user>/config/uptime-kuma/data:/app/data \
--v /home/<user>/config/uptime-kuma/app:/app \
 --name uptime-kuma \
 --network proxy \
 --restart=always \
